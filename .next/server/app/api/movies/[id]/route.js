0a18cdd68f9e2e588f072fbfd00738dab13e6cea
kimer <kimerseng9@gmail.com>
2026-03-29 22:00:53 +0700

    update in from

--- .next/server/app/api/movies/[id]/route.js
+++ .next/server/app/api/movies/[id]/route.js
@@ -1,5 +1,5 @@
 var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/movies/[id]/route.js")
-R.c("server/chunks/[root-of-the-server]__f36aa6bd._.js")
+R.c("server/chunks/[root-of-the-server]__1d815ab6._.js")
 R.c("server/chunks/[root-of-the-server]__f408c708._.js")
 R.c("server/chunks/_next-internal_server_app_api_movies_[id]_route_actions_4cfbe1cf.js")
 R.m(35633)
--- .next/server/app/api/movies/route.js
+++ .next/server/app/api/movies/route.js
@@ -1,5 +1,5 @@
 var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/movies/route.js")
-R.c("server/chunks/[root-of-the-server]__75c027a3._.js")
+R.c("server/chunks/[root-of-the-server]__e3c9bf79._.js")
 R.c("server/chunks/[root-of-the-server]__f408c708._.js")
 R.c("server/chunks/_next-internal_server_app_api_movies_route_actions_0312db74.js")
 R.m(50282)
